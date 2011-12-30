f658b2fb782da6973225dce3c69bd1ae4de2a6a0
Kevin Hansen <kajigga@gmail.com>
2011-12-30 09:41:59 -0700

    MathJax (http://http://www.mathjax.org) customization branding file to the examples

---
+++ Branding/Customizations/enable_mathjax/enable_mathjax.js
@@ -0,0 +1,9 @@
+
+jQuery(function($){
+  var mj = document.createElement('script');
+  mj.type = 'text/javascript';
+  mj.async = true;
+  mj.src = 'https://d3eoax9i5htok0.cloudfront.net/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML';
+  
+  s.parentNode.insertBefore(mj, s);
+});
